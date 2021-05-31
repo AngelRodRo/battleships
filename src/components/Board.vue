f28d3cadd2b179b703c6cdce52357a3b0beb32ef
Alejandro <aleangrodriguez123@gmail.com>
2021-05-31 11:26:30 -0500

    chore: adding board prototype

---
+++ src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div
+      :key="idx1"
+      v-for="(element1, idx1) in matrix">
+      <div
+        :key="idx2"
+        v-for="(element2, idx2) in element1[idx1]"
+      >
+      </div>
+    </div>
+  </div>
+ 
+</template>
+<script>
+  export default {
+    name: 'Board',
+    props: {
+      matrix: {
+        type: Array,
+        default: []
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
