781e4fa7172b40506f451e74402f8fa4b43803e4
Alejandro <aleangrodriguez123@gmail.com>
2021-06-16 21:54:00 -0500

    misc: adding semicolons

--- src/router/index.js
+++ src/router/index.js
@@ -24,13 +24,13 @@ const routes = [
     name: 'History',
     component: History
   },
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
   if (to.path === '/board') {
@@ -44,6 +44,6 @@ router.beforeEach((to, from, next) => {
   }
 
   next();
-})
+});
 
-export default router
+export default router;
